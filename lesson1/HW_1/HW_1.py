6c68252ec82c10dcd8b51062890acc3bd913828e
Ramis <sir.ramis@mail.ru>
2024-03-25 23:55:47 +0300

    added test6_deleit_item_in_cart

--- lesson1/HW_1/HW_1.py
+++ lesson1/HW_1/HW_1.py
@@ -26,7 +26,7 @@ def test_auth_negativ():
 
     browser.quit()
 
-def test_add_item():
+def test3_add_item():
     browser.get('https://www.saucedemo.com/')
 
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
@@ -42,7 +42,7 @@ def test_add_item():
     # product = browser.find_element(By.XPATH, '//a[@id="item_4_title_link"]')
     # assert product is not None, 'count of products does not correspond to added'
 
-def test_deleit_item():
+def test4_deleit_item():
     browser.get('https://www.saucedemo.com/')
 
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
@@ -58,7 +58,7 @@ def test_deleit_item():
     browser.quit()
 
 
-def test_added_item_in_cart():
+def test5_added_item_in_cart():
     browser.get('https://www.saucedemo.com/')
 
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
@@ -68,9 +68,25 @@ def test_added_item_in_cart():
     time.sleep(2)
     browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[2]/button').click()# Добавляет в карзину из карточки
     time.sleep(2)
-    # browser.find_element(By.XPATH, '//*[@id="remove-sauce-labs-backpack"]').click()  # Удаляет с корзины
-    #
-    # assert not browser.find_elements(By.XPATH, '//*[@id="shopping_cart_container"]/a/span'), 'В корзине есть товары'
-    #
-    # time.sleep(4)
+
+    assert browser.find_elements(By.XPATH, '//*[@id="shopping_cart_container"]/a/span'), 'В корзине нет товаров'
+
+    time.sleep(4)
+    browser.quit()
+
+def test6_deleit_item_in_cart():
+    browser.get('https://www.saucedemo.com/')
+
+    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
+    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+    browser.find_element(By.XPATH, '//*[@id="login-button"]').click() # Логинится
+    browser.find_element(By.XPATH, '//*[@id="item_4_img_link"]').click()# Проходит в карточку
+    time.sleep(2)
+    browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[2]/button').click()# Добавляет в карзину из карточки
+    time.sleep(2)
+    browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[2]/button').click()  # Удаляет с корзины через карточку товара
+
+    assert not browser.find_elements(By.XPATH, '//*[@id="shopping_cart_container"]/a/span'), 'В корзине есть товары'
+
+    time.sleep(4)
     browser.quit()
